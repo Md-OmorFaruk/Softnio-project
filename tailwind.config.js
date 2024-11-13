5380538796d5dcdaa648bbc98d015b3c34d47f69
unknown <omor4498@gmail.com>
2024-11-13 17:47:33 +0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html", // Add your HTML files here if you have other file types
+    "./src/**/*.{html,js}", // Or your custom folder structure for components
+  ],
   theme: {
     extend: {},
   },
